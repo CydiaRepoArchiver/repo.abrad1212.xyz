b738fd7e2a314893dd870d1686309222a5cfa692
abrad1212 <abrad12122@gmail.com>
2016-11-23 06:33:53 +0000

    Updates

--- repo.sh
+++ repo.sh
@@ -1,55 +1,29 @@
 #!/bin/bash
 
-script_dir=$(dirname $0)
-cur=$(pwd)
+DepictionFile="depic.txt"
+DepictionFileContents="`cat $DepictionFile`"
 
-cd "$script_dir"
-if [ $# -eq 0 ]; then
-  echo "Usage: $0 <command>" >&2
-  exit 1
-fi
-
-if [ "$1" == "add" ]; then
-	if [ "$#" -ne 3 ]; then
-	  echo "Usage: $0 add <package> <deb file>" >&2
-	  exit 1
-	fi
-	echo "Adding new package...";
-
-	cp "$3" "$script_dir/debs/$2.deb"
-	echo "$script_dir"
-
-elif [ "$1" == "update" ]; then
-	echo "Updating packaging...";
-else
-	echo "Usage: $0 <command>" >&2
-	exit 1
-fi
-
-rm Packages.bz2
 rm Packages
+rm Packages.bz2
+rm Packages.gz
 
 for deb in debs/*.deb
 do
-	echo "Processing $deb...";
-  dpkg-deb -f "$deb" >> Packages
-  ar x "$deb" >> Packages
-  md5sum "$deb" | echo "MD5sum: $(awk '{ print $1 }')" >> Packages
-  wc -c "$deb" | echo "Size: $(awk '{ print $1 }')" >> Packages
-  echo "Filename: $deb" >> Packages
-  dpkg-deb -f "$deb" Package | echo "Depiction: https://$(head -n 1 CNAME)/depictions/?p=$(xargs -0)" >> Packages
-  echo "" >> Packages
-done
 
-echo "" >> Packages; ## Add extra new line
+	echo "Processing $deb...";
+	dpkg-deb -f "$deb" >> Packages
+	ar x "$deb" >> Packages
+	md5sum "$deb" | echo "MD5sum: $(awk '{ print $1 }')" >> Packages
+    wc -c "$deb" | echo "Size: $(awk '{ print $1 }')" >> Packages
+    echo "Filename: $deb" >> Packages
+    
+    echo -n "Enter The Depiction Folder Name > "
+    read Depic
+    
+    dpkg-deb -f "$deb" Package | echo "Depiction: https://$(head -n 1 CNAME)/depictions/$Depic/" >> Packages
+	echo "" >> Packages
+
+done
 
 bzip2 < Packages > Packages.bz2
 gzip -9c < Packages > Packages.gz
-
-git add -A
-now=$(date +"%I:%M %m-%d-%Y")
-git commit -am "Packages Update - $now"
-git push
-
-echo "Updated Github repository with latest packages";
-cd "$cur"
